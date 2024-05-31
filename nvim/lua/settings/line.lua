71177c52920450ef7e0d7059dab86751653d3581
Ilya <ilyaivaniuta@gmail.com>
2024-05-31 16:23:14 +0300

    changing lualine

--- nvim/lua/settings/line.lua
+++ nvim/lua/settings/line.lua
@@ -21,13 +21,32 @@ require("lualine").setup({
 	},
 	sections = {
 		-- lualine_a = { "mode" },
-        lualine_a = { { 'mode', separator = { left = '' }, right_padding = 2 } },
-		lualine_b = { "branch", "diff", "diagnostics" },
-		lualine_c = { "filename" },
-		lualine_x = { "encoding", "fileformat", "filetype" },
-		lualine_y = { "progress" },
+		lualine_a = {
+			{
+				"mode",
+				separator = { left = "" },
+				right_padding = 2,
+			},
+		},
+		lualine_b = { "branch" },
+		lualine_c = {
+			"filename",
+			{
+				"diff",
+				colored = true, -- Displays a colored diff status if set to true
+				symbols = { added = " ", modified = "󰝤 ", removed = " " },
+			},
+		},
+		lualine_x = { "filetype" },
+		lualine_y = { "diagnostics", "progress" },
 		-- lualine_z = { "location" },
-        lualine_z = {{ 'location', separator = { right = '' }, left_padding = 2 },},
+		lualine_z = {
+            {
+                "location",
+                separator = { right = "" },
+                left_padding = 2,
+            }
+        },
 	},
 	inactive_sections = {
 		lualine_a = {},
@@ -36,7 +55,7 @@ require("lualine").setup({
 		lualine_x = {},
 		lualine_y = {},
 		lualine_z = {},
-        -- lualine_z = {{ 'location', separator = { right = '' }, left_padding = 2 },},
+		-- lualine_z = {{ 'location', separator = { right = '' }, left_padding = 2 },},
 	},
 	tabline = {},
 	winbar = {},
